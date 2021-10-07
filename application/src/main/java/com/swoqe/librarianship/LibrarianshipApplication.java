7dba03430990c5830b2b52ad18d4d2201505c9d2
Swoq <misha.kratukl@gmail.com>
2021-10-07 17:48:13 +0300

    Finish renaming application

--- application/src/main/java/com/swoqe/librarianship/LibrarianshipApplication.java
+++ application/src/main/java/com/swoqe/librarianship/LibrarianshipApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringLab2Application {
+public class LibrarianshipApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringLab2Application.class, args);
+        SpringApplication.run(LibrarianshipApplication.class, args);
     }
 
 }
